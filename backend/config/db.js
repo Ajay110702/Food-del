613689353e0d80db40ea2f44d351aa3962ddf80f
Ajay <ajayr5096@gmail.com>
2024-11-26 11:22:34 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDB=async()=>{
+    await mongoose.connect('mongodb+srv://ajayr5096:Oppo123@cluster0.wot19.mongodb.net/food-del').then(()=>console.log("DB connected"));
+}
+
