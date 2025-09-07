98cdddcba8bbec1fb4f67a8373984d5708a4ead6
Ajay <ajayr5096@gmail.com>
2025-09-07 23:41:51 +0530

    Fix Stripe module issue and update dependencies

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -22,22 +22,22 @@ try {
 
      const line_items=req.body.items.map((item)=>({
    price_data:{
-    currency:"Inr",
+    currency:"inr",
     product_data:{
         name:item.name
     },
-    unit_amount:item.price*100*84
+    unit_amount:item.price*100
    },
    quantity:item.quantity,
      }))
 
      line_items.push({
         price_data:{
-            currency:"Inr",
+            currency:"inr",
             product_data:{
                 name:"Delivery Charges"
             },
-            unit_amount:2*100*84
+            unit_amount:2*100
         },
         quantity:1
      })
